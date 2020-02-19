6c24194baef80dae0ff547a6708f90d9f81f3912
Dennis Macharia <dennismacharia4747@gmail.com>
2020-02-19 11:50:48 +0300

    get book by id

--- main.go
+++ main.go
@@ -33,6 +33,18 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 
 // Get a single book
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+
+	// Loop through books and find with id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(&Book{})
 
 }
 
