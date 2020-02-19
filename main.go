e7b3410002b95ac0afb46c9fd0835b9dfeaa3a47
Dennis Macharia <dennismacharia4747@gmail.com>
2020-02-19 11:02:07 +0300

    add Book and Author Model

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Book struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author struct (Model)
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
 func main() {
 	// Init router
 	router := mux.NewRouter()
