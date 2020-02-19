f5eb3afde8e6f8835047821c4dc773f48194a0cf
Dennis Macharia <dennismacharia4747@gmail.com>
2020-02-19 11:03:34 +0300

    add different routes with respective functions

--- main.go
+++ main.go
@@ -21,9 +21,41 @@ type Author struct {
 	Lastname  string `json:"lastname"`
 }
 
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Get a single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Update a book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Delete a book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	// Init router
 	router := mux.NewRouter()
+
+	// Route handlers / Endpoint
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
